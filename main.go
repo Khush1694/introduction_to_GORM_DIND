620ce24444d8bb7f36580fcaf208f4c4fed2a12c
PacoDw <__pm@outlook.com>
2019-10-29 11:39:49 -0600

    :tada: added db connection and gin

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"github.com/PacoDw/introduction_to_GORM/db"
+	"github.com/PacoDw/introduction_to_GORM/models"
+	"github.com/PacoDw/introduction_to_GORM/routes"
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	// Get the database connection
+	db := db.GetConnection()
+	defer db.Close()
+
+	// Enable the log showing detailed log
+	db.LogMode(true)
+
+	// Migrate the Models to the database
+	// db.Debug().DropTableIfExists(&models.User{}) // drop db table
+	db.Debug().AutoMigrate(&models.User{})
+}
+
+func main() {
+	// Set the router as the default one shipped with Gin
+	router := gin.Default()
+
+	// Serve frontend static files
+	// router.Use(static.Serve("/", static.LocalFile("./views", true)))
+
+	// Setup route group for the user
+	routes.User(router.Group("/user"))
+
+	router.Run(":3000")
+}
