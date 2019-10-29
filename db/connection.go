6ad05babe2310e5f148d6e970108bf76021a7b54
PacoDw <__pm@outlook.com>
2019-10-29 11:35:35 -0600

    :card_file_box: changed a comment

--- db/connection.go
+++ db/connection.go
@@ -10,7 +10,7 @@ import (
 	_ "github.com/joho/godotenv/autoload"     // auto env variables import
 )
 
-// GetConnection returns the database connection
+// GetConnection returns the db connection
 func GetConnection() (db *gorm.DB) {
 	// Set the database connection
 	db, err := gorm.Open("mysql",
